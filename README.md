16bb049128920659b7f00d5953913b9ed4838cf7
Clayton Collins <claytonccollins@gmail.com>
2017-01-20 11:10:06 -0800

    establish basic functionality with minimal styling

---
+++ README.md
@@ -0,0 +1,12 @@
+###Ping Pong
+#Clayton Collins
+
+
+####Takes a number and returns it with multiples of 3,5, and 15 changed in to fun surprises
+
+
+##Behaviours
+#1. populate an array with numbers approaching the users input
+#2. check and replace all multiples of 15
+#3. check and replace all multiples of 5
+#4. check and replace all multiples of 3
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+    number = parseInt($('#input').val());
+    console.log(number);
+    populator(number);
+    
+  })
+  ////////GLOBAL VARIABLES
+  var resultarray = []
+
+
+
+  ////////FUNCTIONS
+  var populator = function(number) {
+    for (var i=0;i<number;i++) {
+      if ((i+1) % 15 === 0) {
+        resultarray[i]=("ping-pong")
+        $('#list').append("<li>"+"ping-pong"+"</li>")
+      } else if ((i+1) % 5 === 0) {
+        resultarray[i]=("pong")
+        $('#list').append("<li>"+"pong"+"</li>")
+      } else if ((i+1) % 3 === 0) {
+        resultarray[i]=("ping")
+        $('#list').append("<li>"+"ping"+"</li>")
+      } else {
+        resultarray[i]=((i+1))
+        $('#list').append("<li>"+(i+1)+"</li>")
+      }
+    }
+  }
+});
