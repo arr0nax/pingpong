1913b0455634f117a0cb69f0289c02275bfdeab9
Clayton Collins <claytonccollins@gmail.com>
2017-01-20 14:26:29 -0800

    add more styling features, trying to change behavior of list display so that it replaces itself rather than adds on

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ $(function() {
   $('form').submit(function(event) {
     event.preventDefault();
     number = parseInt($('#input').val());
-    console.log(number);
     populator(number);
     $('body').css("background-image", "url(img/clapping1.gif)");
     $('.result').show();
-    bounceright();
+
   })
   ////////GLOBAL VARIABLES
   var resultarray = []
+  var bounce = 1
 
 
 
@@ -18,27 +18,46 @@ $(function() {
     for (var i=0;i<number;i++) {
       if ((i+1) % 15 === 0) {
         resultarray[i]=("ping-pong")
-        $('#list').append("<li>"+"ping-pong"+"</li>")
+        // $('#list').append("<li>"+"ping-pong"+"</li>")
       } else if ((i+1) % 5 === 0) {
         resultarray[i]=("pong")
-        $('#list').append("<li>"+"pong"+"</li>")
+        // $('#list').append("<li>"+"pong"+"</li>")
       } else if ((i+1) % 3 === 0) {
         resultarray[i]=("ping")
-        $('#list').append("<li>"+"ping"+"</li>")
+        // $('#list').append("<li>"+"ping"+"</li>")
       } else {
         resultarray[i]=((i+1))
-        $('#list').append("<li>"+(i+1)+"</li>")
+        // $('#list').append("<li>"+(i+1)+"</li>")
       }
     }
-    $('.arrays').append(resultarray+" ");
+    $('.arrays').append(resultarray);
+    var array = resultarray.join('<br/>')
+    $('#list2').text(array);
+  }
+
+  var bouncearound = function() {
+    console.log(bounce);
+    if (bounce%2 != 0) {
+      bounceright();
+      bounce = bounce + 1;
+    } else {
+      bounceleft();
+      bounce = bounce + 1;
+    }
+    $('#bouncecount').text(bounce);
   }
 
   var bounceright = function() {
-    $('.arrays').animate({marginLeft: "90vw"},'slow','swing',bounceleft())
+    $('.arrays').animate({marginLeft: "120vw"},'slow')
   }
 
   var bounceleft = function(array) {
-    $('.arrays').animate({marginRight: "90vw"},'slow','swing',bounceright())
+    $('.arrays').animate({marginLeft: "-100vw"},'slow')
   }
 
+  /////////GLOBAL FUNCTIONS
+
+    $('body').keypress(function(){
+      bouncearound();
+    })
 });
